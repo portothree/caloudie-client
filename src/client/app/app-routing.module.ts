8c47b20bb1f2b9cb6b51a40c2776ec4cdbb4a6fe
Gustavo Porto <portothree@gmail.com>
2020-02-15 18:08:03 +0000

    :lipstick: styling home page, routes and global styles

--- src/client/app/app-routing.module.ts
+++ src/client/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { FoodComponent } from './components/food/food.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: 'food',
+		component: FoodComponent,
+	},
+	{
+		path: '',
+		pathMatch: 'full',
+		component: HomeComponent,
+	},
+	{
+		path: '**',
+		redirectTo: '',
+	},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -3,16 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FoodComponent } from './components/food/food.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, HomeComponent, HeaderComponent, FoodComponent],
+	imports: [BrowserModule, AppRoutingModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
